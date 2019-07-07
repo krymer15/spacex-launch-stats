b2266e66bfd8bdfd1fbe2d59d9b6ff1295bf52ad
krymer15 <18199459+krymer15@users.noreply.github.com>
2019-07-06 23:19:53 -0400

    final commit before heroku push

---
+++ public/precache-manifest.f722b64d144a57416409c014483122dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27761fd6d61f0dd501079dee60602cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1cde871e8d499a3ad7f",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "cc670ab25507c0682e52",
+    "url": "/static/js/2.ac48c7be.chunk.js"
+  },
+  {
+    "revision": "d1cde871e8d499a3ad7f",
+    "url": "/static/js/main.070392e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6d783ad24a18f9e4d4ae401b3bf6ec88",
+    "url": "/static/media/Spacex24.6d783ad2.jpg"
+  }
+]);
